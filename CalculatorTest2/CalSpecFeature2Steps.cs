7cdfb8c971c212dd752b7e16a8bf69b00386d2be
subrahp <psubrahmanya@spiderlogic.com>
2016-11-08 17:55:09 +0530

    Second Feature File added

---
+++ CalculatorTest2/CalSpecFeature2Steps.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace CalculatorTest2
+{
+    [Binding]
+    public class CalSpecFeature2Steps
+    {
+        [Given(@"We have entered (.*) into the calculator")]
+        public void GivenWeHaveEnteredIntoTheCalculator(int x)
+        {
+            int a = x;
+            Console.WriteLine("First number is" + a);
+        }
+        
+        [Given(@"We also have entered (.*) into the calculator")]
+        public void GivenWeAlsoHaveEnteredIntoTheCalculator(int y)
+        {
+            int b = y;
+            Console.WriteLine("Second number is" + b);
+        }
+        
+        [When(@"We perform add operation")]
+        public void WhenWePerformAddOperation()
+        {
+            Console.WriteLine("Calculation");
+        }
+        
+        [Then(@"Output of adding (.*) and (.*) should be (.*) on the screen")]
+        public void ThenOutputOfAddingAndShouldBeOnTheScreen(int x, int y, int result)
+        {
+            int a = x;
+            int b = y;
+            int E = x + y;
+            Assert.AreEqual(E, result);
+            Console.Out.WriteLine("Test Completed");
+        }
+    }
+}
---
+++ CalculatorTest2/CalcSecondFeature.feature
@@ -0,0 +1,16 @@
+﻿Feature: CalSpecFeature2
+Add two numbers
+
+@SmokeTest
+Scenario Outline: Here we do addition of Numbers
+Given We have entered <x> into the calculator
+And We also have entered <y> into the calculator
+When We perform add operation
+Then Output of adding <x> and <y> should be <result> on the screen
+
+Scenarios: addition
+| x | y | result|
+| 1 | 2 | 3|
+| 2 | 2 | 4|
+| 5 | 4 | 9|
+| 100 | 200 | 300|
